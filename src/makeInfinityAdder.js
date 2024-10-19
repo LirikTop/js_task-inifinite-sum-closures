6c4130d2cf3e4cd7352ec39f924e0af62335dea1
LirikTop <melnicukkiril2002@gmail.com>
2024-10-19 20:30:26 +0300

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,22 +4,20 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  let flag = 0;
   let sum = 0;
 
   const makeSum = (a) => {
     if (typeof a === 'number') {
       sum += a;
-      flag = 0;
 
       return makeSum;
-    } else if (typeof a !== 'number') {
-      flag++;
-
-      sum = flag === 2 ? 0 : sum;
-
-      return sum;
     }
+
+    const result = sum;
+
+    sum = 0;
+
+    return result;
   };
 
   return makeSum;
